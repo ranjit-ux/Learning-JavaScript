6dd92b66d6fd6579afa81a48e314465e50a3940b
Ranjit Kumar Singh <ranjitksingh.079@gmail.com>
2025-06-10 12:35:51 +0530

    Lecture 07: DOM 2

---
+++ DOM 2/script.js
@@ -0,0 +1,33 @@
+// let div=document.querySelector("div");
+// console.log(div)
+
+// let id=div.getAttribute("id");
+// console.log(id);
+
+// div.setAttribute("id","newdiv");
+
+
+let div=document.querySelector("div");
+div.style.backgroundColor="green";
+
+
+/*
+for creating new element in JS
+
+let <name> = document.CreateElement("<tag name like div,button,p etc>")
+let el=document.createElement("div")
+*/
+
+/*
+
+for insert elements to main file
+node.append(el)-->adds at the end of node (inside)
+node.prepend(el)-->adds at the start of node (inside)
+node.before(el)-->adds before the node (outside)
+node.after(el)--> adds after the node (outside)
+
+
+for delete element
+node.remove()-->remove the node
+
+*/
